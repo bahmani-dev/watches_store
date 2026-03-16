e787126ef32de465f5615adf9404d704b67d7727
bahmani-dev <mohammadmandegar2019@gmail.com>
2026-03-16 16:33:09 +0430

    edit: Make sections parts for other files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { HeroSection } from "../sections/HeroSection";
 
 export const Home = () => {
-  return <div className="text-black">Home</div>;
+  return (
+    <>
+      <div className="text-black">
+        <HeroSection />
+        <section>featured</section>
+        <section>our story</section>
+        <section>products</section>
+        <section>testimonials</section>
+        <section>new arrivals</section>
+        <section>email</section>
+        <section>footer</section>
+      </div>
+    </>
+  );
 };
